dc18c51133c1f30a25ce6e2e5737c15fe332aed6
shefo05 <shafeelgendy@gmail.com>
2025-12-31 20:25:56 +0200

    add bonus.js

---
+++ Bonus.js
@@ -0,0 +1,13 @@
+var majorityElement = function(nums) {
+    let candidate = null;
+    let count = 0;
+
+    for (let num of nums) {
+        if (count === 0) {
+            candidate = num;
+        }
+        count += (num === candidate) ? 1 : -1;
+    }
+
+    return candidate;
+};
